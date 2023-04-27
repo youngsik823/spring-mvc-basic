39cc8a325edff573f492988fb9e4a6dc60ff0dc9
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-27 15:36:06 +0900

    커밋커밋

---
+++ src/test/java/com/spring/mvc/mybatis/PersonMapperTest.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.mybatis;
+
+import com.spring.mvc.jdbc.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PersonMapperTest {
+
+    @Autowired
+    PersonMapper mapper;
+
+    @Test
+    @DisplayName("마이바티스 매퍼로 사람정보 저장에 성공해야 한다.")
+    void saveTest() {
+        //given
+        Person p = Person.builder()
+                .personName("김마바")
+                .personAge(44)
+                .build();
+        // when
+        boolean flag = mapper.save(p);
+
+        //then
+        assertTrue(flag);
+    }
+
+}
