a35c4c38205801b95f2adfdb32f95495c9f50107
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-27 15:20:27 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/mybatis/PersonMapper.java
+++ src/main/java/com/spring/mvc/mybatis/PersonMapper.java
@@ -1,7 +1,13 @@
 package com.spring.mvc.mybatis;
 
+import com.spring.mvc.jdbc.Person;
+
 public interface PersonMapper {
 
     // CRUD에 대한 기능 명세
-    //
+    // 사람 정보 저장
+    boolean save(Person p);
+    
+    // 사람 정부 수정
+    change
 }
