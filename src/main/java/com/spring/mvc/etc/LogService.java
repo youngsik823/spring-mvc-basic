ff8b5064c59435824feef85c1cb69913b23e4a19
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-27 14:23:32 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/etc/LogService.java
+++ src/main/java/com/spring/mvc/etc/LogService.java
@@ -8,6 +8,14 @@ public class LogService {
     /*
         - 로그: 발생시간, 로그 레벨, 파일저장
         - 로그 라이브러리: logback, log4;, slf4j
+
+        - 로그 레벨
+        1. trace : 애플리케이션의 실행흐름 세부정보, 디버깅 목적
+        2. debug :
+        3. info
+        4. warn
+        5. error
+        6. fatal
      */
 
     public void showLog() {
