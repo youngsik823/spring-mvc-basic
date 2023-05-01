a1cc96af8b276fac2fad40feb208db4f86959915
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-05-01 16:43:01 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap05/dto/page/PageMaker.java
+++ src/main/java/com/spring/mvc/chap05/dto/page/PageMaker.java
@@ -7,9 +7,9 @@ import lombok.ToString;
 public class PageMaker {
 
     // 한번에 그려낼 페이지 수
-    // 1~ 5, 6 ~ 10
-    // 1~ 10, 11~ 20
-    private  static final int PAGE_COUNT = 5;
+    // 1 ~ 5, 6 ~ 10
+    // 1 ~ 10, 11 ~ 20
+    private static final int PAGE_COUNT = 5;
 
     // 화면 렌더링시 페이지의 시작값과 끝값
     private int begin, end;
@@ -19,16 +19,15 @@ public class PageMaker {
 
     // 현재 요청 페이지 정보
     private Page page;
-    // 외부에서 받아와야한다. (클라)
 
     // 총 게시물 수
     private int totalCount;
-    // 외부에서 받아야한다. (DB)
 
 
     public PageMaker(Page page, int totalCount) {
         this.page = page;
         this.totalCount = totalCount;
+        makePageInfo();
     }
 
     // 페이지 계산 알고리즘
@@ -36,28 +35,22 @@ public class PageMaker {
 
         // 1. end값 계산
         // 올림처리 (현재 위치한 페이지번호 / 한 화면에 배치할 페이지수 ) *  한 화면에 배치할 페이지 수
-        this.end = (int) (Math.ceil(page.getPageNo() / (double)PAGE_COUNT) * PAGE_COUNT);
+        this.end = (int) Math.ceil(page.getPageNo() / (double)PAGE_COUNT) * PAGE_COUNT;
 
         // 2. begin값 계산
         this.begin = this.end - PAGE_COUNT + 1;
 
         /*
-
         - 총 게시물수가 237개고, 한 화면당 10개의 게시물을 배치하고 있다면
           페이지 구간은
-
           1 ~ 10페이지 구간 : 게시물 100개
           11 ~ 20페이지 구간: 게시물 100개
           21 ~ 24페이지 구간: 게시물 37개
-
         - 마지막 페이지 구간에서는 보정이 필요함.
-
         - 마지막 구간 끝페이지 보정 공식:
           올림처리(총 게시물 수 / 한 페이지당 배치할 게시물 수)
-
          */
-
-        int realEnd = (int) Math.ceil(totalCount / (double)page.getAmount());
+        int realEnd = (int) Math.ceil((double)totalCount / page.getAmount());
 
         // 마지막 페이지 구간에서만 엔드보정이 일어나야 함
         if (realEnd < this.end) this.end = realEnd;
@@ -69,4 +62,5 @@ public class PageMaker {
         this.next = end < realEnd;
 
     }
+
 }
