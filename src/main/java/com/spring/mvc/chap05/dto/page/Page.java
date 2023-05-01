544cca97ec4356113fe524f7a837e7c21b318ebd
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-05-01 16:40:27 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap05/dto/page/Page.java
+++ src/main/java/com/spring/mvc/chap05/dto/page/Page.java
@@ -8,23 +8,38 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Page {
-    
+
     private int pageNo; // 클라이언트가 보낸 페이지 번호
     private int amount; // 클라이언트가 보낸 목록게시물 수
 
-    public int getPageStart() {
-        /*
-            pageNo : 1 -> return 0
-            pageNo : 2 -> return 6
-            pageNo : 3 -> return 12
-            pageNo : n -> return (n-1) * 6
-         */
-        return (pageNo - 1) * amount;
-
-    }
     public Page() {
         this.pageNo = 1;
-        this.amount= 6;
+        this.amount = 6;
     }
 
+    public void setPageNo(int pageNo) {
+        if (pageNo < 1 || pageNo > Integer.MAX_VALUE) {
+            this.pageNo = 1;
+            return;
+        }
+        this.pageNo = pageNo;
+    }
+
+    public void setAmount(int amount) {
+        if (amount < 6 || amount > 10) {
+            this.amount = 6;
+            return;
+        }
+        this.amount = amount;
+    }
+
+    public int getPageStart() {
+        /*
+            pageNo : 1 ->  return 0
+            pageNo : 2 ->  return 6
+            pageNo : 3 ->  return 12
+            pageNo : n ->  return (n - 1) * 6
+         */
+        return (pageNo - 1) * amount;
+    }
 }
