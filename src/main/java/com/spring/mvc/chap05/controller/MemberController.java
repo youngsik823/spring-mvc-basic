24ef4e382d4c3461f056ba1f83b622a64ee64ce8
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-05-10 10:34:31 +0900

    특별히 커밋해드릴게요~~!!!

--- src/main/java/com/spring/mvc/chap05/controller/MemberController.java
+++ src/main/java/com/spring/mvc/chap05/controller/MemberController.java
@@ -4,10 +4,12 @@ import com.spring.mvc.chap05.dto.SignUpRequestDTO;
 import com.spring.mvc.chap05.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Slf4j
@@ -30,5 +32,14 @@ public class MemberController {
     log.info("/members/sign-up POST ! - {}", dto);
         boolean flag = memberService.join(dto);
 
+    }
+
+    // 아이디, 이메일 중복검사
+    // 비동기 요청 처리 (REST 방식)
+    @GetMapping("/check")
+    @ResponseBody
+    public ResponseEntity<?> check(String type, String keyword) {
+        log.info("/members/check?type={}&keyword={} ASYNC GET!", type, keyword);
+        return null;
     }
 }
